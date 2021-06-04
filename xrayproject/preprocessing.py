3a26bb116fe5b16de31e1c0454e7be37e94d9858
Kim Hedelin <kimhedelin@gmail.com>
2021-06-04 10:54:19 +0200

    added preprocessing

---
+++ xrayproject/preprocessing.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask = tf.cast(input_mask, tf.float32) / 255.0
+    return input_image, input_mask
+
+
+def flip_resize(image, mask):
+    input_image = tf.image.resize(image, (2897, 2499))
+    input_mask = tf.image.resize(mask, (2897, 2499))
+
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    input_image_flipped = tf.image.flip_left_right(input_image)
+    input_mask_flipped = tf.image.flip_left_right(input_mask)
+
+    return input_image, input_mask, input_image_flipped, input_mask_flipped
+
+
+def resize_test(image, mask):
+    input_image = tf.image.resize(image, (2897, 2499))
+    input_mask = tf.image.resize(mask, (2897, 2499))
+
+    input_image, input_mask = normalize(image, input_mask)
+    return input_image, input_mask
