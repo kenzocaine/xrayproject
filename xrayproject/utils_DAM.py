90c6236918148c4f3f579ebabe55737b89efd1d0
David McGady <82802695+dmcgady196884@users.noreply.github.com>
2021-06-07 12:41:12 +0200

    Not interesting

--- xrayproject/utils_DAM.py
+++ xrayproject/utils_DAM.py
@@ -5,7 +5,21 @@ import numpy as np
 import PIL
 from PIL import Image
 
-def load_pngs(n=1, get_all=False, get_target=False, get_random = True, balanced = True, path = ''):
+def load_mask_names(path = ''):
+    # This function obtains the list of image-IDs for images that have masks:
+    list_of_mask_filenames = get_filenames(path)
+    masked_IDs = list(set([ int(os.path.basename(masked_img).split('_')[1]) \
+        for masked_img in list_of_mask_filenames]))
+    return masked_IDs
+
+def load_masked_img_names(path = ''):
+    # This function obtains the list of image-filepaths which have masks:
+    list_of_img_filenames = get_filenames(path)
+    list_of_masked_img_numbers = load_mask_names(path)
+    return masked_IDs
+
+
+def load_pngs(n=1, get_all=False, get_target=False, get_random=True, balanced=True, path=''):
     # Load png and returns them as list of  img (tensor) , targets (bol)
     # If balanced = True, will attempt to divide n into equal parts of positive and negative samples
     # If random, will choose random images
@@ -17,12 +31,14 @@ def load_pngs(n=1, get_all=False, get_target=False, get_random = True, balanced 
     list_of_filenames = get_filenames(path)
     list_of_images = []
     targets = []
+    ID = []
     if get_all:
         for file in list_of_filenames:
             image = load_png(file)
             list_of_images.append(image)
             targets.append(int(file[-5]))
-        return list_of_images, targets
+            ID.append(int(os.path.basename(file).split('_')[1]))
+        return list_of_images, targets, ID
 
     # Extract positive and negative samples
     positive, negative = [], []
@@ -36,7 +52,7 @@ def load_pngs(n=1, get_all=False, get_target=False, get_random = True, balanced 
         if n == 1:
             rand_index = random.randint(0, len(list_of_filenames))
             file = list_of_filenames[rand_index]
-            return load_png(file), file[-5]
+            return load_png(file), int(file[-5]), os.path.basename(file).split('_')[1]
         if balanced:
             pos = int(n/2)
             neg = int(n - pos)
@@ -45,22 +61,42 @@ def load_pngs(n=1, get_all=False, get_target=False, get_random = True, balanced 
             for i in rand_list_pos:
                 list_of_images.append(load_png(positive[i]))
                 targets.append(int(positive[i][-5]))
+                ID.append(int(os.path.basename(positive[i]).split('_')[1]))
             for i in rand_list_neg:
                 list_of_images.append(load_png(negative[i]))
                 targets.append(int(negative[i][-5]))
-            return list_of_images, targets
+                ID.append(int(os.path.basename(negative[i]).split('_')[1]))
+            return list_of_images, targets, ID
         else:
             rand_list = [random.randint(0, len(list_of_filenames)) for i in range(n)]
             for i in rand_list:
                 list_of_images.append(load_png(list_of_filenames[i]))
                 targets.append(int(list_of_filenames[i][-5]))
-            return list_of_images, targets
+                ID.append(int(os.path.basename(list_of_filenames[i]).split('_')[1]))
+            return list_of_images, targets, ID
 
     for file in list_of_filenames[0:n]:
         image = load_png(file)
         list_of_images.append(image)
         targets.append(int(file[-5]))
-    return list_of_images, targets
+        ID.append(int(os.path.basename(file).split('_')[1]))
+    return list_of_images, targets, ID
+
+
+def load_masks(path, ID):
+    # Returns the mask (tensor), ID (int)
+    list_of_masks = list(range(len(ID)))
+    list_of_filenames = get_filenames(path)
+    for file in list_of_filenames:
+        file_ID = int(os.path.basename(file).split('_')[1])
+        for index, I_D in enumerate(ID):
+            if file_ID == I_D:
+                list_of_masks[index] = load_png(file)
+    return list_of_masks, ID
+
+
+def spurious_funct():
+    return "Does this exist? (I am not Camus. (Really. (Bugz-n-suqidz.)))"
 
 
 def get_filenames(path):
@@ -74,6 +110,7 @@ def get_filenames(path):
                 list_of_filenames.append(os.path.join(dirname, filename))
     return list_of_filenames
 
+
 def load_png(file):
     image = tf.io.read_file(file)
     image = tf.io.decode_png(image)
@@ -81,12 +118,6 @@ def load_png(file):
 
 if __name__ == '__main__':
     print(load_png(0))
-
-def spurious_funct_2():
-    return "WTF?"
-
-def spurious_funct():
-    return "Does this exist? (I am not Camus. (Really. (Bugz-n-suqidz.))) Futz."
 
 def get_img_heights(path):
     list_of_filenames = get_filenames(path)
@@ -102,5 +133,3 @@ def get_img_sizes(path):
     list_of_filenames = get_filenames(path)
     img_sizes = np.array([Image.open(file).size for file in list_of_filenames])
     return img_sizes
-
-
