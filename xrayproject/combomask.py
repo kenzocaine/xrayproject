605e4cb5155fa7f0265c92fd1e747f64407d3607
Christian Hed <ch@Christians-MBP.localdomain>
2021-06-09 18:02:12 +0200

    combomaskfile updated

--- xrayproject/combomask.py
+++ xrayproject/combomask.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def combine_left_right(left_mask, right_mask):
     index = list(range(0, (len(left_mask))))
     mask = []
@@ -5,3 +7,10 @@ def combine_left_right(left_mask, right_mask):
         combination = np.maximum(left_mask[i], right_mask[i])
         mask.append(combination)
     return mask
+
+def USA_data(path, path_right, path_left):
+    left,targets, ID = load_masks(10, get_all = False, get_random = False, balanced = True, path = path_left)
+    right, ID = load_train(ID, path_right)
+    USA_masks = combine_left_right(left, right)
+    USA_images, ID = load_train(ID, path)
+    return USA_masks, USA_images
