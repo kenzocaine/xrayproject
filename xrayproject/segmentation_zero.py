15817a24f3aa959e45e5575db313817ca21b84c0
David McGady <82802695+dmcgady196884@users.noreply.github.com>
2021-06-14 11:54:30 +0200

    Be wary of the 2nd argument in the initialize_unet(self, input_shape)-function

--- xrayproject/segmentation_zero.py
+++ xrayproject/segmentation_zero.py
@@ -13,55 +13,98 @@ class UNET():
         self.model = []
         self.channels = input_shape[-1]
 
-    def initialize_unet(self):
+    def initialize_unet(self, input_shape):
         print('Initializing model...')
-        unet = Sequential()
+
         inputs = Input(input_size)
 
-        unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        unet.add(MaxPooling2D(pool_size=(2, 2)))
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-        unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-        unet.add(MaxPooling2D(pool_size=(2, 2)))
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-        unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-        unet.add(MaxPooling2D(pool_size=(2, 2)))
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-        unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-        unet.add(MaxPooling2D(pool_size=(2, 2)))
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-        unet.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
 
-        unet.add(concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3))
-        unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
 
-        unet.add(concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3))
-        unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
 
-        unet.add(concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3))
-        unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
 
-        unet.add(concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3))
-        unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
 
-        unet.add(Conv2D(1, (1, 1), activation='sigmoid'))
+        conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+        unet = Model(inputs=[inputs], outputs=[conv10])
+
+
+        # unet = Sequential()
+        # inputs = Input(input_size)
+
+        # unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        # unet.add(MaxPooling2D(pool_size=(2, 2)))
+
+        # unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        # unet.add(MaxPooling2D(pool_size=(2, 2)))
+
+        # unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        # unet.add(MaxPooling2D(pool_size=(2, 2)))
+
+        # unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        # unet.add(MaxPooling2D(pool_size=(2, 2)))
+
+        # unet.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+
+        # unet.add(concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3))
+        # unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+
+        # unet.add(concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3))
+        # unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+
+        # unet.add(concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3))
+        # unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+
+        # unet.add(concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3))
+        # unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        # unet.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+
+        # unet.add(Conv2D(1, (1, 1), activation='sigmoid'))
 
         unet.compile(Adam(lr=0.001),
                      loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
                      metrics=['accuracy'])
 
-        my_callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss',
-                                                         mode='min',
-                                                         patience=10)]
+        # my_callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+        #                                                  mode='min',
+        #                                                  patience=10)]
 
         self.model = unet
 
